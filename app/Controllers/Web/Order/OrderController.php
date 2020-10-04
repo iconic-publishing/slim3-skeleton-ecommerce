ae14633f232b571e149a5e362f9693a2c0b1bfb3
John <john@custom-golf-bags.com>
2020-10-04 13:22:49 +0700

    OrderController Updates

--- app/Controllers/Web/Order/OrderController.php
+++ app/Controllers/Web/Order/OrderController.php
@@ -133,15 +133,15 @@ class OrderController extends BaseConstructor {
             );
             
             try {
-				$amount = number_format($total, 2);
-				$amount = str_replace(',', '', $amount);
+                $amount = number_format($total, 2);
+                $amount = str_replace(',', '', $amount);
                 $amount = str_replace('.', '', $amount);
                 
                 $charge = Charge::create([
-					'amount' => $amount,
-					'currency' => $this->config->get('stripe.currency'),
-					'source' => $stripeToken,
-					'description' => 'Online Order ' . $order->order_id
+                    'amount' => $amount,
+                    'currency' => $this->config->get('stripe.currency'),
+                    'source' => $stripeToken,
+                    'description' => 'Online Order ' . $order->order_id
                 ]);
                 
                 $event = new OrderWasCreated($order, $this->basket);
@@ -172,49 +172,49 @@ class OrderController extends BaseConstructor {
                 $event->dispatch();
 
                 $this->mail->to($order->user->email_address, $this->config->get('mail.from.name'))->send(new WebOrderConfirmation($order));
-				$this->mail->to($this->config->get('company.email'), $this->config->get('mail.from.name'))->send(new AdminOrderConfirmation($order));
+                $this->mail->to($this->config->get('company.email'), $this->config->get('mail.from.name'))->send(new AdminOrderConfirmation($order));
 
                 return $response->withRedirect($this->router->pathFor('getShowOrder', compact('hash')));
             } catch (InvalidRequest $e) {
-				$this->flash->addMessage('error', $this->config->get('messages.payment.checkout.invalid'));
-				return $response->withRedirect($this->router->pathFor('getOrder'));
-			} catch (Card $e) {
-				$id = Charge::all(['limit' => 1])->data[0]['id'];
-				$name = Charge::all(['limit' => 1])->data[0]['source']['name'];
-				$last4 = Charge::all(['limit' => 1])->data[0]['source']['last4'];
-				$brand = Charge::all(['limit' => 1])->data[0]['source']['brand'];
-				$address_zip_check = Charge::all(['limit' => 1])->data[0]['source']['address_zip_check'];
-				$cvc_check = Charge::all(['limit' => 1])->data[0]['source']['cvc_check'];
-				$zip = Charge::all(['limit' => 1])->data[0]['source']['address_zip'];
-				$funding = Charge::all(['limit' => 1])->data[0]['source']['funding'];
-				$status = Charge::all(['limit' => 1])->data[0]['status'];
-				$failure_message = Charge::all(['limit' => 1])->data[0]['failure_message'];
-				$risk_level = Charge::all(['limit' => 1])->data[0]['outcome']['risk_level'];
-				
-				$event = new OrderWasCreated($order, $this->basket);
-				$event->attach(new RecordFailedPayment(
-					$id,
-					$name,
-					$last4,
-					$brand,
-					$address_zip_check,
-					$cvc_check,
-					$zip,
-					$funding,
-					$status,
-					$failure_message,
-					$risk_level
-				));
-				$event->dispatch();
-				
-				$this->mail->to($this->config->get('company.email'), $this->config->get('mail.from.name'))->send(new AdminFailedPayment($order));
+                $this->flash->addMessage('error', $this->config->get('messages.payment.checkout.invalid'));
+                return $response->withRedirect($this->router->pathFor('getOrder'));
+            } catch (Card $e) {
+                $id = Charge::all(['limit' => 1])->data[0]['id'];
+                $name = Charge::all(['limit' => 1])->data[0]['source']['name'];
+                $last4 = Charge::all(['limit' => 1])->data[0]['source']['last4'];
+                $brand = Charge::all(['limit' => 1])->data[0]['source']['brand'];
+                $address_zip_check = Charge::all(['limit' => 1])->data[0]['source']['address_zip_check'];
+                $cvc_check = Charge::all(['limit' => 1])->data[0]['source']['cvc_check'];
+                $zip = Charge::all(['limit' => 1])->data[0]['source']['address_zip'];
+                $funding = Charge::all(['limit' => 1])->data[0]['source']['funding'];
+                $status = Charge::all(['limit' => 1])->data[0]['status'];
+                $failure_message = Charge::all(['limit' => 1])->data[0]['failure_message'];
+                $risk_level = Charge::all(['limit' => 1])->data[0]['outcome']['risk_level'];
+                
+                $event = new OrderWasCreated($order, $this->basket);
+                $event->attach(new RecordFailedPayment(
+                    $id,
+                    $name,
+                    $last4,
+                    $brand,
+                    $address_zip_check,
+                    $cvc_check,
+                    $zip,
+                    $funding,
+                    $status,
+                    $failure_message,
+                    $risk_level
+                ));
+                $event->dispatch();
+                
+                $this->mail->to($this->config->get('company.email'), $this->config->get('mail.from.name'))->send(new AdminFailedPayment($order));
 
-				$this->flash->addMessage('error', $e->getMessage());
-				return $response->withRedirect($this->router->pathFor('order', compact('hash')));
-			}
+                $this->flash->addMessage('error', $e->getMessage());
+                return $response->withRedirect($this->router->pathFor('order', compact('hash')));
+            }
 		} else {
-			$this->flash->addMessage('error', $this->config->get('messages.recaptcha.error'));
-			return $response->withRedirect($this->router->pathFor('getOrder'));
+            $this->flash->addMessage('error', $this->config->get('messages.recaptcha.error'));
+            return $response->withRedirect($this->router->pathFor('getOrder'));
         }
     }
 
